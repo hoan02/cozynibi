6c48dd441a13f3c8cf0681a80459e10d7cc31258
hoanit02 <hoanvipboi1@gmail.com>
2023-04-19 22:06:31 +0700

    match login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,85 +1,46 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import { ToastContainer } from "react-toastify";
-// import { Helmet } from "react-helmet";
-
-// import "react-toastify/dist/ReactToastify.css";
-
-// import DashboardAdmin from "./pages/DashboardAdmin";
-// import PagesManagement from "./pages/PagesManagement";
-// import Sidebar from "./components/Sidebar";
-// import "./App.css";
-
-// const routes = [
-//   {
-//     path: "/",
-//     element: <DashboardAdmin />,
-//     title: "Home",
-//   },
-//   {
-//     path: "/pages_management",
-//     element: <PagesManagement />,
-//     title: "Pages Management",
-//   },
-// ];
-
-// const App = () => {
-//   return (
-//     <div className="app">
-//       <Sidebar>
-//         <Routes>
-//           {routes.map((route) => (
-//             <Route
-//               key={route.path}
-//               exact
-//               path={route.path}
-//               element={
-//                 <>
-//                   <Helmet>
-//                     <title>{route.title}</title>
-//                   </Helmet>
-//                   {route.element}
-//                 </>
-//               }
-//             />
-//           ))}
-//         </Routes>
-//       </Sidebar>
-//       <ToastContainer autoClose={2000} draggablePercent={60} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import React from "react";
+import React, { useState } from "react";
 // import 'antd/dist/antd.css'
 import { Menu } from "antd";
-import "./App.css";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import Dashboard from "./pages/DashboardAdmin";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import toastService from "./utils/toastService";
+import newRequest from "./utils/newRequest";
+import Login from "./pages/Login";
 import DashboardAdmin from "./pages/DashboardAdmin";
-import AboutUsManagement from './components/layout/AboutUsManagement'
-import ContactManagement from './components/layout/ContactManagement'
+import AboutUsManagement from "./components/layout/AboutUsManagement";
+import ContactManagement from "./components/layout/ContactManagement";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  console.log(currentUser);
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      flex: 1,
-      height: '100vh',
-    }}>
-      <Header/>
-      <div className="sidebar-container">
-        <SideMenu />
-        <Content />
-      </div>
-      <Footer/>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        height: "100vh",
+      }}
+    >
+      <Header />
+
+      {currentUser ? (
+        <div className="sidebar-container">
+          <SideMenu />
+          <Content />
+        </div>
+      ) : (
+        <Login />
+      )}
+      <Footer />
+      <ToastContainer autoClose={2000} draggablePercent={60} />
     </div>
   );
 };
@@ -87,11 +48,23 @@ const App = () => {
 function SideMenu() {
   const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    try {
+      await newRequest.post("auth/logout");
+      localStorage.setItem("currentUser", null);
+      window.location.reload();
+    } catch (err) {
+      toastService.error(err);
+    }
+  };
+
   return (
     <div>
       <Menu
         onClick={({ key }) => {
-          if (key === "signout") {
+          if (key === "logout") {
+            handleLogout();
+            navigate("/");
           } else {
             navigate(key);
           }
@@ -99,12 +72,16 @@ function SideMenu() {
         defaultSelectedKeys={[window.location.pathname]}
         items={[
           { label: "Home", key: "/" },
-          { label: "Pages", key: "/pages_management", children: [
-            {label: 'About us', key: '/about_us_management'},
-            {label: 'Contact', key: '/contact_management'},
-          ]},
+          {
+            label: "Pages",
+            key: "/pages_management",
+            children: [
+              { label: "About us", key: "/about_us_management" },
+              { label: "Contact", key: "/contact_management" },
+            ],
+          },
           { label: "Account", key: "" },
-          { label: "Logout", danger: true },
+          { label: "Logout", key: "logout", danger: true },
         ]}
       ></Menu>
     </div>
@@ -115,10 +92,9 @@ function Content() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<DashboardAdmin />}/>
-        <Route path="/about_us_management" element={<AboutUsManagement />}/>
-        <Route path="/contact_management" element={<ContactManagement />}/>
-        <Route path="/" element={<DashboardAdmin />}/>
+        <Route path="/" element={<DashboardAdmin />} />
+        <Route path="/about_us_management" element={<AboutUsManagement />} />
+        <Route path="/contact_management" element={<ContactManagement />} />
       </Routes>
     </div>
   );
