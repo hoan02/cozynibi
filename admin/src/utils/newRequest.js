75615d4b812a594d1c1d51050c85ac1e5e0395d7
hoanit02 <hoanvipboi1@gmail.com>
2023-04-23 15:50:03 +0700

    remove admin

--- admin/src/utils/newRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const newRequest = axios.create({
-  baseURL: "http://localhost:8080/api/",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
-});
-
-export default newRequest;
