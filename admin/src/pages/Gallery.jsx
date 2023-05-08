a416f7c0548852f8418d24745e15b50b8ea3de31
hoanit02 <hoanvipboi1@gmail.com>
2023-05-08 22:17:38 +0700

    completed gallery admin page

--- admin/src/pages/Gallery.jsx
+++ admin/src/pages/Gallery.jsx
@@ -1,12 +1,15 @@
 import { Box } from "@mui/material";
-
+import CreateShowcase from "../components/showcaseControl/CreateShowcase";
+import BoardShowcase from "../components/showcaseControl/BoardShowcase";
 
 const Gallery = () => {
   return (
     <Box sx={{ padding: "20px" }}>
       <h1 style={{ textAlign: "center", color: "green" }}>GALLERY</h1>
+      <CreateShowcase />
+      <BoardShowcase />
     </Box>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
